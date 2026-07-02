524c77d54eafdca9a46feba804769937438e200b
phihash <insharp0220@gmail.com>
2026-07-03 01:07:37 +0900

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Link href={"/ui"}>UIPage</Link>
       <p>とぷ</p>
       思ったことを書く
       <hr />
